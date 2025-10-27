ae67f0e4b2efadb9389fa6370e91be67c54bd3f6
tanvidudhagundi <tanvi.dudhagundi@gmail.com>
2025-10-27 12:00:45 +0530

    Update expenses.h

--- expenses.h
+++ expenses.h
@@ -6,14 +6,8 @@
 struct Expense {
     char category[20];
     float amount;
-    char description[50];
-    char date[15]; // DD/MM/YYYY
 };
 
-void addExpense(struct Expense expenses[], int *count);
-void viewExpenses(struct Expense expenses[], int count);
 void monthlySummary(struct Expense expenses[], int count);
-void saveExpenses(struct Expense expenses[], int count, char username[]);
-void loadExpenses(struct Expense expenses[], int *count, char username[]);
 
 #endif
